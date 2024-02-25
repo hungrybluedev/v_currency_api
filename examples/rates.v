a5b116f855322ea6e0ca85d598a2a6cd54f38140
Subhomoy Haldar <hungrybluedev@gmail.com>
2024-02-25 17:10:50 +0000

    refactor: Make errors panic in example.

--- examples/rates.v
+++ examples/rates.v
@@ -6,31 +6,22 @@ import os
 fn main() {
 	client := freecurrencyapi_v.APIClient{
 		api_key: os.getenv_opt('FREE_CURRENCY_API_KEY') or {
-			eprintln('Please set the environment variable FREE_CURRENCY_API_KEY')
-			return
+			panic('Please set the environment variable FREE_CURRENCY_API_KEY')
 		}
 	}
 
-	status := client.get_status() or {
-		eprintln('Failed to get status with error:\n${err}')
-		return
-	}
+	status := client.get_status() or { panic('Failed to get status with error:\n${err}') }
 
 	println(status)
 
-	monthly_quota := status.quotas['month'] or {
-		eprintln('Failed to get monthly quota')
-		return
-	}
+	monthly_quota := status.quotas['month'] or { panic('Failed to get monthly quota') }
 
 	if monthly_quota.remaining <= 4500 {
-		eprintln('Monthly quota exhausted for CI runs. Please run this example locally or use your own API key.')
-		return
+		panic('Monthly quota exhausted for CI runs. Please run this example locally or use your own API key.')
 	}
 
 	currencies := client.get_currencies() or {
-		eprintln('Failed to get currencies with error:\n${err}')
-		return
+		panic('Failed to get currencies with error:\n${err}')
 	}
 
 	println('Currencies retrieved successfully:\n')
@@ -39,8 +30,7 @@ fn main() {
 	}
 
 	rates := client.get_latest(base_currency: 'GBP', currencies: ['INR', 'USD', 'EUR']) or {
-		eprintln('Failed to get latest rates with error:\n${err}')
-		return
+		panic('Failed to get latest rates with error:\n${err}')
 	}
 	println('Latest rates retrieved successfully:\n${rates}')
 }
